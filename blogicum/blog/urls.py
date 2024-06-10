b170c77a5102a1254aff3a11d43137270e90b485
nikita-git <nikita.maximov@gmail.com>
2024-06-10 15:50:04 +0300

    Доработка шаблона в соответствии с ТЗ

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import index, post_detail, category_posts
+from .views import index, post_detail, category_post
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('category/<slug:category_slug>/', category_posts, name='category_posts'),
+    path('category/<slug:category_slug>/', category_post, name='category_post'),
     path('posts/<int:id>/', post_detail, name='post_detail'),
     path('', index, name='index'),
 ]
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -59,7 +59,7 @@ def post_detail(request, id):
     return render(request, template, context)
 
 
-def category_posts(request, category_slug):
+def category_post(request, category_slug):
     template = 'blog/category.html'
     context = {'slug': category_slug}
     return render(request, template, context)
