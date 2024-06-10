6d2760ec67ac85d322556528090299a44f46eeb4
nikita-git <nikita.maximov@gmail.com>
2024-06-10 16:27:08 +0300

    Исправление замечаний Flake8

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,7 +5,9 @@ from .views import index, post_detail, category_post
 app_name = 'blog'
 
 urlpatterns = [
-    path('category/<slug:category_slug>/', category_post, name='category_post'),
+    path('category/<slug:category_slug>/',
+         category_post,
+         name='category_post'),
     path('posts/<int:id>/', post_detail, name='post_detail'),
     path('', index, name='index'),
 ]
